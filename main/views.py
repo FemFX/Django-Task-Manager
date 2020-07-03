6a240686add213b1604d1c37cc22070387083834
FemFX <ignedko34@gmail.com>
2020-07-03 10:07:45 +0300

    Create views.py

---
+++ main/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+# Create your views here.
+
+
+def index(request):
+    tasks = Task.objects.order_by('-id').all()
+    return render(request, 'main/index.html', {'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+   
+    form = TaskForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'main/create.html', context)
